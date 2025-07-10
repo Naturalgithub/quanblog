c19895ea9f01ba43abe6f943c9492e32c6610701
yuanquanke <3318615021@qq.com>
2025-07-10 10:07:12 +0800

    feat: 新增博客主题配置文件用于自定义样式和布局

--- docs/.vitepress/blog-theme.ts
+++ docs/.vitepress/blog-theme.ts
@@ -29,9 +29,10 @@ const blogTheme = getThemeConfig({
 
   // 页脚
   footer: {
-    // message 字段支持配置为HTML内容，配置多条可以配置为数组
-    // message: '下面 的内容和图标都是可以修改的噢（当然本条内容也是可以隐藏的）',
-    copyright: 'MIT License | 粥里有勺糖',
+    // message 字段支持配置为HTML内容，配置多条可以配置为数组  网站运行时间
+    // message: '',
+    copyright: 'yuanquanke 2018 - 2025',
+    version: false,
     icpRecord: {
       name: '鄂ICP备2021014112号-1',
       link: 'https://beian.miit.gov.cn/'
